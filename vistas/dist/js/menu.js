0b2ca044ffe481fdd2e0d99d033ccf6ea3153a38
angel-alvarez-guzman <angel.alvarez.guzman0@gmail.com>
2021-08-06 17:16:18 -0500

    scroll menu

--- vistas/dist/js/menu.js
+++ vistas/dist/js/menu.js
@@ -14,35 +14,18 @@ $(document).on("click",".icono-menu", function(){
 */
 window.addEventListener('scroll',function(){
 
-<<<<<<< HEAD
     var header = document.querySelector('.navbar');
     header.classList.toggle ('sticky', window.scrollY > 0 );
-=======
-   var header = document.querySelector('.scrol');
-   header.classList.toggle ('sticky', window.scrollY > 0 );
->>>>>>> parent of a16285e (borrado de archivos)
+
 
 });
 
-$(window).on('scroll', function(){
-<<<<<<< HEAD
-    
-});
-
-function toggleMune() {
-    var menuToggle = document.querySelector('.toggle');
-    var menu = document.querySelector('.menu-items');
-    menuToggle.classList.toggle('active');
-    menu.classList.toggle('menu');
-=======
-   
-});
 
 function toggleMune() {
    var menuToggle = document.querySelector('.toggle');
    var menu = document.querySelector('.menu-items');
    menuToggle.classList.toggle('active');
    menu.classList.toggle('menu');
->>>>>>> parent of a16285e (borrado de archivos)
+
 }
 
