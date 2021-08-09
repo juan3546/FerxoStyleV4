d1ba1efa763f9f4361bcbac17e58c377aef5d682
angel-alvarez-guzman <angel.alvarez.guzman0@gmail.com>
2021-08-09 15:30:51 -0500

    Filtrado en archivo productos.php

--- modelos/rutas.php
+++ modelos/rutas.php
@@ -2,13 +2,13 @@
 
 class Ruta{
     public function ctrRuta(){
-        return "http://localhost/FerxoStyleV4/";
-        //return "http://localhost/FerxoStyleV4/FerxoStyleV4/";
+        //return "http://localhost/FerxoStyleV4/";
+        return "http://localhost/FerxoStyleV4/FerxoStyleV4/";
        // return "https://ferxostyle.com.mx/";
     }
 
     public function ctrRutaServidor(){
     //    return "https://admin.ferxostyle.com.mx/";
-    return "http://localhost/ferxostyle/";
+    return "http://localhost/ferxostyle/FerxoStyle/";
     }
 }
--- vistas/modulos/productos.php
+++ vistas/modulos/productos.php
@@ -3,9 +3,83 @@ $url =  Ruta::ctrRuta();
 $servidor =  Ruta::ctrRutaServidor();
 $ruta = $rutas[0];
 ?>
- <!-- PRODUCTS -->
- <!-- Acordeon para filtrado -->
- <section class="container-fluid" id="acordeonFiltrado">
- 
+<!-- filtrado  -->
+<div class="container filtrado">
+    <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+        <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12">
+            <button class="btn btn-primary btnFiltro" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                Categorias
+                <i class="fas fa-bars"></i>
+            </button>
+            <input type="text"class="form-control" placeholder="Buscar">
+        </div>
+    </div>
+</div>
+
+
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+  </div>
+</div>
+<!-- cards de productos  -->
+<section class="container-fluid mt-5" id="">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card tarjetasss mt-3">
+                <div class="product-1 align-items-center p-2 text-center"> 
+                    <img src="vistas/img/plantilla/Modelo2.png" class="rounded" alt="" width="100%">
+                    <h5 class="mt-2">Modelo 1</h5>
+                    <div class="cost mt-3 text-dark">;
+                        <span>$500</span>;
+                   </div>
+                </div>
+                <!-- button -->
+                <div class="p-3 shoe text-center text-white mt-3 cursor">
+                    <span class="text-uppercase botonCarrito">Agregar a pedido</span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card tarjetass mt-3">
+                <div class="product-1 align-items-center p-2 text-center"> 
+                    <img src="vistas/img/plantilla/Modelo3.png" class="rounded" alt="" width="100%">
+                    <h5 class="mt-2">Modelo 1</h5>
+                    <div class="cost mt-3 text-dark">;
+                        <span>$500</span>;
+                   </div>
+                </div>
+                <!-- button -->
+                <div class="p-3 shoe text-center text-white mt-3 cursor">
+                    <span class="text-uppercase botonCarrito">Agregar a pedido</span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card tarjetass mt-3">
+                <div class="product-1 align-items-center p-2 text-center"> 
+                    <img src="vistas/img/plantilla/Modelo4.png" class="rounded" alt="" width="100%">
+                    <h5 class="mt-2">Modelo 1</h5>
+                    <div class="cost mt-3 text-dark">;
+                        <span>$500</span>;
+                   </div>
+                </div>
+                <!-- button -->
+                <div class="p-3 shoe text-center text-white mt-3 cursor">
+                    <span class="text-uppercase botonCarrito">Agregar a pedido</span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+    </div>
  </section>
-<!-- fin del acordeon para filtrado -->
+
+ 
