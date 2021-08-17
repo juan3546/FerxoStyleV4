2db81606aaaaf2b6ad46f5b5be35c8269689f45b
Juanjo <juan.cisneros3546@gmail.com>
2021-08-16 19:24:29 -0500

    cambios en detalle de producto

--- vistas/modulos/detalle-del-producto.php
+++ vistas/modulos/detalle-del-producto.php
@@ -21,8 +21,19 @@ $ruta = $rutas[0];
    <p class="mt-3"><h4>Despricion del Producto <br></h4><br>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus repudiandae nesciunt odio, magni deleniti, minima saepe maxime beatae quidem ipsum nobis laborum aut nulla alias commodi hic. Inventore, rem dolor.
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, delectus! Quaerat facere, itaque excepturi eligendi fuga commodi modi. Tempora autem nobis pariatur dolore consequuntur sunt, earum soluta repudiandae est voluptates.</p>
-  </div>
+
+    <div class="row">
+    <div class="col-6">
+       <button class="btn btn-primary">Solicitar Pedido</button>
+    </div>
+    <div class="col-6">
+        .<button class="btn btn-primary"> <i class="fas fa-cart-plus"></i> Agregar a Carrito</button>
+    </div>
  </div>
+  </div>
+ 
+ </div>
+ 
 </section>
 <!-- apartado para los comentarios, articulos relacionados -->
 <section class="container-fluid mt-5">
@@ -43,6 +54,14 @@ $ruta = $rutas[0];
   <div class="container mt-3 d-flex justify-content-center">
     <div class="row d-flex justify-content-center">
         <div class="col-md-12">
+        <div class="card p-3 mb-2">
+          <div class="container">
+            <div class="row col-12">
+              <textarea name="" id="comentarioArea" cols="15" rows="5" placeholder="Agrega tu comentario"></textarea> 
+              <button class="btn btn-primary">Agregar Comentario</button>
+            </div>
+          </div>
+            </div>
             <div class="card p-3 mb-2">
                 <div class="d-flex flex-row"> <img src="https://i.imgur.com/dwiGgJr.jpg" height="40" width="40" class="rounded-circle">
                     <div class="d-flex flex-column ms-2">
@@ -61,12 +80,8 @@ $ruta = $rutas[0];
                         <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lectus nibh, efficitur in bibendum id, pellentesque quis nibh. Ut dictum facilisis dui, non faucibus dolor sit amet lorem auctor vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque risus mauris</p>
                     </div>
                 </div>
-                <div class="d-flex justify-content-between">
-                    <div class="d-flex flex-row gap-3 align-items-center">
-                        <div class="d-flex align-items-center"> <i class="fa fa-heart-o"></i> <span class="ms-1 fs-10">Like</span> </div>
-                        <div class="d-flex align-items-center"> <i class="fa fa-comment-o"></i> <span class="ms-1 fs-10">Comments</span> </div>
-                    </div>
-                    <div class="d-flex flex-row"> <span class="text-muted fw-normal fs-10">May 21,2020 1:10 PM</span> </div>
+                <div class="d-flex justify-content-end">
+                    <div class="d-flex flex-row"> <span class="text-muted fw-normal fs-10">May 22,2020 12:10 PM</span> </div>
                 </div>
             </div>
             <div class="card p-3 mb-2">
@@ -76,12 +91,8 @@ $ruta = $rutas[0];
                         <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lectus nibh, efficitur in bibendum id, pellentesque quis nibh. Ut dictum facilisis dui, non faucibus dolor sit amet lorem auctor vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque risus mauris</p>
                     </div>
                 </div>
-                <div class="d-flex justify-content-between">
-                    <div class="d-flex flex-row gap-3 align-items-center">
-                        <div class="d-flex align-items-center"> <i class="fa fa-heart-o"></i> <span class="ms-1 fs-10">Like</span> </div>
-                        <div class="d-flex align-items-center"> <i class="fa fa-comment-o"></i> <span class="ms-1 fs-10">Comments</span> </div>
-                    </div>
-                    <div class="d-flex flex-row"> <span class="text-muted fw-normal fs-10">May 12,2020 12:10 PM</span> </div>
+                <div class="d-flex justify-content-end">
+                    <div class="d-flex flex-row"> <span class="text-muted fw-normal fs-10">May 22,2020 12:10 PM</span> </div>
                 </div>
             </div>
             <div class="card p-3 mb-2">
@@ -91,11 +102,7 @@ $ruta = $rutas[0];
                         <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lectus nibh, efficitur in bibendum id, pellentesque quis nibh. Ut dictum facilisis dui, non faucibus dolor sit amet lorem auctor vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque risus mauris</p>
                     </div>
                 </div>
-                <div class="d-flex justify-content-between">
-                    <div class="d-flex flex-row gap-3 align-items-center">
-                        <div class="d-flex align-items-center"> <i class="fa fa-heart-o"></i> <span class="ms-1 fs-10">Like</span> </div>
-                        <div class="d-flex align-items-center"> <i class="fa fa-comment-o"></i> <span class="ms-1 fs-10">Comments</span> </div>
-                    </div>
+                <div class="d-flex justify-content-end">
                     <div class="d-flex flex-row"> <span class="text-muted fw-normal fs-10">May 22,2020 12:10 PM</span> </div>
                 </div>
             </div>
@@ -104,7 +111,70 @@ $ruta = $rutas[0];
 </div>
   </div>
   <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-  <h2>Relacionados</h2>
+   <!-- cards para mostrar articulos relacionados -->
+   <div class="container-fluid bg-trasparent my-4 p-3" style="position: relative;">
+        <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+            
+      <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="card-sl h-100">
+        <div class="card-image">
+          <img src="vistas/img/plantilla/modelo2.png" />
+        </div>
+           <a class="card-action" href="#"><i class="fas fa-cart-plus"></i></a>
+        <div class="card-heading">
+            <h4>Modelo prueba</h4>
+        </div>
+        <div class="card-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo alias facilis accusantium ipsum accusamus reprehenderit inventore obcaecati. Fuga facilis
+        </div>
+        <div class="card-text">
+          $400
+
+        </div>
+          <a href="#" class="card-button"> Solicitar pedido</a>
+       </div>
+     </div>
+     <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="card-sl h-100">
+        <div class="card-image">
+          <img src="vistas/img/plantilla/modelo3.png" />
+        </div>
+           <a class="card-action" href="#"><i class="fas fa-cart-plus"></i></a>
+        <div class="card-heading">
+            <h4>Modelo prueba</h4>
+        </div>
+        <div class="card-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo alias facilis accusantium ipsum accusamus reprehenderit inventore obcaecati. Fuga facilis
+        </div>
+        <div class="card-text">
+          $400
+
+        </div>
+          <a href="#" class="card-button"> Solicitar pedido</a>
+       </div>
+     </div>
+     <div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">
+      <div class="card-sl h-100">
+        <div class="card-image">
+          <img src="vistas/img/plantilla/modelo4.png" />
+        </div>
+           <a class="card-action" href="#"><i class="fas fa-cart-plus"></i></a>
+        <div class="card-heading">
+            <h4>Modelo prueba</h4>
+        </div>
+        <div class="card-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo alias facilis accusantium ipsum accusamus reprehenderit inventore obcaecati. Fuga facilis
+        </div>
+        <div class="card-text">
+          $400
+
+        </div>
+          <a href="#" class="card-button"> Solicitar pedido</a>
+       </div>
+     </div>
+        </div>
+    </div>
+   <!-- fin cards para mostrar articulos relacionados -->
   </div>
   <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
   <h2>Ofertas</h2>
