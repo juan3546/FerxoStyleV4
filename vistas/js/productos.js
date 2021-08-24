bfb235d9c2b130319c9d3ef5a832b059006d7ac9
angel-alvarez-guzman <angel.alvarez.guzman0@gmail.com>
2021-08-24 18:03:03 -0500

    Mostrar tallas en cards

--- vistas/js/productos.js
+++ vistas/js/productos.js
@@ -27,6 +27,7 @@ $(document).on("keyup", "#txtArticulosHombre", function(ev){
                 if(respuesta){
                     for (let i = 0; i < respuesta.length; i++) {
                         $(".mostrarProductoHombre").html("");
+                        $(".pagination").html("");
                         codigoHtml += '<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">';
                         codigoHtml += '<div class="card card2">';
                         codigoHtml += '<div class="img-container">';
@@ -54,47 +55,13 @@ $(document).on("keyup", "#txtArticulosHombre", function(ev){
                         codigoHtml += '<div class="d-flex justify-content-between align-items-center mt-2">';
                         codigoHtml += '<div class="size"> ';
 
-                        
+                        codigoHtml += '<div class="tallas'+i+'"></div>';
                         
                         //MOSTRAR LAS TALLAS DEL ARTICULO
 
-                        tallas(respuesta[i]["id"]);
-                        //codigoHtml += '<span>'+i+'</span>';
-                        /*
-                        var datosTalla = new FormData();
+                        tallas(respuesta[i]["id"], ('tallas'+i));
 
-                        datosTalla.append("tallas", "");
-                        datosTalla.append("articulo", respuesta[i]["id"]);
-                         $.ajax({
-                            url: rutaOculta+"ajax/productos.ajax.php",
-                            method:"POST",
-                            data: datosTalla,
-                            cache: false,
-                            contentType: false,
-                            processData: false,
-                            dataType: "json",
-                            success:function(respuestaTalla){
-                                var codigoHtmlTalla = '';
-                                $(".size").html("");
-                                for (let i = 0; i < respuestaTalla.length; i++) {
-                                    
-                                    codigoHtmlTalla += '<label class="radio">';
-                                    codigoHtmlTalla += '<input type="radio" name="size1" value="small"> ';
-                                    codigoHtmlTalla += '<span>'+respuestaTalla[i]["talla"]+'</span>';
-                                    codigoHtmlTalla += '</label> ';
-                                    
-                                }
-                                $(".size").append(codigoHtmlTalla);
-                                
-                    
-                    
-                            }
-                    
-                        });
-                        */
 
-                        //codigoHtml += '<span>15</span> </label> ';
-                        
                         codigoHtml += '</div>';
                         codigoHtml += '</div>';
                         codigoHtml += '<div class="mt-3"> <button class="btn btn-block form-control botonCarrito text-white" >Agregar a carrito</button> </div>';
@@ -146,29 +113,45 @@ $(document).on("keyup", "#txtArticulosMujeres", function(){
                         $(".pagination").html("");
                         $(".mostrarProductoMujeres").html("");
                         codigoHtml += '<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">';
-                        codigoHtml += '<div class="card-sl h-100">';
-                        codigoHtml += '<div class="card-image">';
-                        codigoHtml += '<img src="'+rutaOcultaServidor+respuesta[i]["foto"]+'" />';
+                        codigoHtml += '<div class="card card2">';
+                        codigoHtml += '<div class="img-container">';
+                        codigoHtml += '<div class="d-flex justify-content-between align-items-center p-2 first">';
+                        if(rutaOcultaServidor+respuesta[i]["estado"] == "Nuevo"){
+                            codigoHtml += '<span class="percent">Nuevo</span>';
+                        }else if(rutaOcultaServidor+respuesta[i]["estado"] == "Oferta"){
+                            codigoHtml += '<span class="ofert">Oferta</span>';
+                        }
                         codigoHtml += '</div>';
-                        codigoHtml += '<a class="card-action" href="#"><i class="fas fa-cart-plus"></i></a>';
-                        codigoHtml += '<div class="card-heading">';
-                        codigoHtml += respuesta[i]["nombre"];
+                        
+                        codigoHtml += '<img src="'+rutaOcultaServidor+respuesta[i]["foto"]+'" class="img-fluid" />';
                         codigoHtml += '</div>';
-                        codigoHtml += '<div class="card-text">';
-                        if(respuesta[i]["descripcion"] != null){
-                            codigoHtml += respuesta[i]["descripcion"];
-                        } 
-                        codigoHtml += '</div>';
-                        codigoHtml += '<div class="card-text">';
+                        codigoHtml += '<div class="product-detail-container">';
+                        codigoHtml += '<div class="d-flex justify-content-between align-items-center">';
+                        codigoHtml += '<h6 class="mb-0">'+respuesta[i]["nombre"]+'</h6> ';
+                        codigoHtml += '<span class="text-danger font-weight-bold">';
                         if(respuesta[i]["precioOferta"] != null){
                             codigoHtml += '<del>$'+respuesta[i]["precio"]+' </del> &nbsp;&nbsp; $'+respuesta[i]["precioOferta"]+'';
                         }else{
                             codigoHtml += '$'+respuesta[i]["precio"];
                         }
-                        codigoHtml += ' </div>';
-                        codigoHtml += '<a href="#" class="card-button"> Solicitar pedido</a>';
+                        codigoHtml += '</span>';
+                        codigoHtml += '</div>';
+                        codigoHtml += '<div class="d-flex justify-content-between align-items-center mt-2">';
+                        codigoHtml += '<div class="size"> ';
+
+                        codigoHtml += '<div class="tallas'+i+'"></div>';
+                        
+                        //MOSTRAR LAS TALLAS DEL ARTICULO
+
+                        tallas(respuesta[i]["id"], ('tallas'+i));
+
+
                         codigoHtml += '</div>';
                         codigoHtml += '</div>';
+                        codigoHtml += '<div class="mt-3"> <button class="btn btn-block form-control botonCarrito text-white" >Agregar a carrito</button> </div>';
+                        codigoHtml += '</div>';
+                        codigoHtml += '</div>';
+                        codigoHtml += '</div>'; 
                     }
                     $(".mostrarProductoMujeres").append(codigoHtml);
     
@@ -210,29 +193,45 @@ $(document).on("keyup", "#txtArticulosInfante", function(){
                         $(".mostrarProductoInfante").html("");
                         $(".pagination").html("");
                         codigoHtml += '<div class="col-sm-12 col-md-6 col-lg-4 col-xl-4">';
-                        codigoHtml += '<div class="card-sl h-100">';
-                        codigoHtml += '<div class="card-image">';
-                        codigoHtml += '<img src="'+rutaOcultaServidor+respuesta[i]["foto"]+'" />';
+                        codigoHtml += '<div class="card card2">';
+                        codigoHtml += '<div class="img-container">';
+                        codigoHtml += '<div class="d-flex justify-content-between align-items-center p-2 first">';
+                        if(rutaOcultaServidor+respuesta[i]["estado"] == "Nuevo"){
+                            codigoHtml += '<span class="percent">Nuevo</span>';
+                        }else if(rutaOcultaServidor+respuesta[i]["estado"] == "Oferta"){
+                            codigoHtml += '<span class="ofert">Oferta</span>';
+                        }
                         codigoHtml += '</div>';
-                        codigoHtml += '<a class="card-action" href="#"><i class="fas fa-cart-plus"></i></a>';
-                        codigoHtml += '<div class="card-heading">';
-                        codigoHtml += respuesta[i]["nombre"];
+                        
+                        codigoHtml += '<img src="'+rutaOcultaServidor+respuesta[i]["foto"]+'" class="img-fluid" />';
                         codigoHtml += '</div>';
-                        codigoHtml += '<div class="card-text">';
-                        if(respuesta[i]["descripcion"] != null){
-                            codigoHtml += respuesta[i]["descripcion"];
-                        } 
-                        codigoHtml += '</div>';
-                        codigoHtml += '<div class="card-text">';
+                        codigoHtml += '<div class="product-detail-container">';
+                        codigoHtml += '<div class="d-flex justify-content-between align-items-center">';
+                        codigoHtml += '<h6 class="mb-0">'+respuesta[i]["nombre"]+'</h6> ';
+                        codigoHtml += '<span class="text-danger font-weight-bold">';
                         if(respuesta[i]["precioOferta"] != null){
                             codigoHtml += '<del>$'+respuesta[i]["precio"]+' </del> &nbsp;&nbsp; $'+respuesta[i]["precioOferta"]+'';
                         }else{
                             codigoHtml += '$'+respuesta[i]["precio"];
                         }
-                        codigoHtml += ' </div>';
-                        codigoHtml += '<a href="#" class="card-button"> Solicitar pedido</a>';
+                        codigoHtml += '</span>';
+                        codigoHtml += '</div>';
+                        codigoHtml += '<div class="d-flex justify-content-between align-items-center mt-2">';
+                        codigoHtml += '<div class="size"> ';
+
+                        codigoHtml += '<div class="tallas'+i+'"></div>';
+                        
+                        //MOSTRAR LAS TALLAS DEL ARTICULO
+
+                        tallas(respuesta[i]["id"], ('tallas'+i));
+
+
                         codigoHtml += '</div>';
                         codigoHtml += '</div>';
+                        codigoHtml += '<div class="mt-3"> <button class="btn btn-block form-control botonCarrito text-white" >Agregar a carrito</button> </div>';
+                        codigoHtml += '</div>';
+                        codigoHtml += '</div>';
+                        codigoHtml += '</div>'; 
                     }
                     $(".mostrarProductoInfante").append(codigoHtml);
     
@@ -246,7 +245,7 @@ $(document).on("keyup", "#txtArticulosInfante", function(){
 
 });
 
-function tallas(articulo){
+function tallas(articulo, div){
     var rutaOculta = $("#rutaOculta").val();
     var datos = new FormData();
     datos.append("tallas", "");
@@ -261,16 +260,16 @@ function tallas(articulo){
         dataType: "json",
         success:function(respuestaTalla){
             var codigoHtmlTalla = '';
-            $(".size").html("");
+            
             for (let i = 0; i < respuestaTalla.length; i++) {
-                
+                $("."+div).html("");
                 codigoHtmlTalla += '<label class="radio">';
                 codigoHtmlTalla += '<input type="radio" name="size1" value="small"> ';
                 codigoHtmlTalla += '<span>'+respuestaTalla[i]["talla"]+'</span>';
                 codigoHtmlTalla += '</label> ';
                 
             }
-            $(".size").append(codigoHtmlTalla);
+            $("."+div).append(codigoHtmlTalla);
 
 
         }
