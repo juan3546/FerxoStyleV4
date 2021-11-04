570ef73c34268304d19f35964ec1cf10344acbef
Juanjo <juan.cisneros3546@gmail.com>
2021-11-04 13:34:23 -0600

    se agrego carrusel para categorias en inicio

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -8,8 +8,8 @@ class Conexion{
 
     {
 
-        $link = new PDO("mysql:host=162.241.60.127;dbname=systems3_ferxostyle","systems3_Angel","Angel?123+");
-       // $link = new PDO("mysql:host=localhost;dbname=ferxostyle","root","root");
+        //$link = new PDO("mysql:host=162.241.60.127;dbname=systems3_ferxostyle","systems3_Angel","Angel?123+");
+        $link = new PDO("mysql:host=localhost;dbname=ferxostyle","root","root");
 
         $link->exec("set names utf8");
 
--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -292,7 +292,7 @@
   <div class="col-12 ">
     <div class="row">
       <div class="col-4 d-flex">
-        <div class="card border-0 m-auto" style="width: 50%">
+        <div class=" border-0 m-auto" style="width: 50%">
            <img src="vistas/img/plantilla/categoria3.jpg" class="card-img-top" alt="...">
            <div class="card-body">
            <p class="card-text">Bufs</p>
@@ -300,7 +300,7 @@
         </div>
       </div>
       <div class="col-4 d-flex">
-      <div class="card border-0 m-auto" style="width: 50%">
+      <div class=" border-0 m-auto" style="width: 50%">
            <img src="vistas/img/plantilla/categoria2.jpg" class="card-img-top" alt="...">
            <div class="card-body">
            <p class="card-text">Sudaderas</p>
@@ -308,7 +308,7 @@
         </div>
       </div>
       <div class="col-4 d-flex">
-      <div class="card border-0 m-auto" style="width: 50%">
+      <div class=" border-0 m-auto" style="width: 50%">
            <img src="vistas/img/plantilla/categoria1.jpg" class="card-img-top" alt="...">
            <div class="card-body">
            <p class="card-text">Jerseys</p>
@@ -317,6 +317,84 @@
       </div>
     </div>
   </div>
+</section>
+<section>
+<div class="container text-center my-3">
+    <h2 class="font-weight-light">Bootstrap Multi Slide Carousel</h2>
+    <div class="row mx-auto my-auto justify-content-center">
+        <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/31f?text=1" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 1</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 2</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 3</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 4</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 5</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 6</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            </a>
+            <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            </a>
+        </div>
+    </div>
+    <h5 class="mt-2 fw-light">advances one slide at a time</h5>
+</div>
 </section>
 <br>
 <br>
